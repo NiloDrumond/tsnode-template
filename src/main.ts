4cbd468b27feb7e4d9183c85a44ee5e399b80ce1
Nilo Drumond <nilobdrumond@gmail.com>
2022-02-01 11:19:03 -0300

    setup

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+// #!/usr/bin/env node
+import './test';
+
+console.log('Hello World');
--- src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import { createUserController } from '@/modules/user/useCases/createUser';
-import { Router } from 'express';
-const userRoutes = Router();
-
-userRoutes.post('/', (request, response) => {
-  return createUserController.handle(request, response);
-});
-
-export { userRoutes };
